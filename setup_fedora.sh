faa5d7b3f2551ecf37111ccd13dab93101dac066
Frank <derp@localhost>
2018-02-26 16:33:02 +1100

    Added more libraries to install

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -1,4 +1,6 @@
 set -euo pipefail
 
 # install desired libraries
-sudo dnf install -y vim-X11 gcc git
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y vim-X11 gcc clang git ffmpeg tmux
+pip3 install --user virtualenv
